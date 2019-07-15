2e5d3a5753fb6955d48904561a8e8de63b33505c
Carlossantana987 <carlossantana987@outlook.com>
2019-07-15 15:38:19 -0400

    Country class has been setup

---
+++ src/src/main/java/com/countriesapp/countries/Country.java
@@ -0,0 +1,69 @@
+package com.countriesapp.countries;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+public class Country
+{
+
+    //Fields or attributes
+    private static final AtomicLong counter = new AtomicLong();
+    private long id;
+    private String cname;
+    private int population;
+    private double landMassSize;
+    private double medianAge;
+
+    public Country(String cname, int population, double landMassSize, double medianAge)
+    {
+        this.id = counter.incrementAndGet();
+        this.cname = cname;
+        this.population = population;
+        this.landMassSize = landMassSize;
+        this.medianAge = medianAge;
+    }
+
+    public long getId()
+    {
+        return id;
+    }
+
+    public String getCname()
+    {
+        return cname;
+    }
+
+    public void setCname(String cname)
+    {
+        this.cname = cname;
+    }
+
+    public int getPopulation()
+    {
+        return population;
+    }
+
+    public void setPopulation(int population)
+    {
+        this.population = population;
+    }
+
+    public double getLandMassSize()
+    {
+        return landMassSize;
+    }
+
+    public void setLandMassSize(double landMassSize)
+    {
+        this.landMassSize = landMassSize;
+    }
+
+    public double getMedianAge()
+    {
+        return medianAge;
+    }
+
+    public void setMedianAge(double medianAge)
+    {
+        this.medianAge = medianAge;
+    }
+}
